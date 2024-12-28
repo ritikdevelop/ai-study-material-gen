2be930d3618cf9d0d011347f10ce9a22abe0491b
ritikdevelop <hrithikrana216@gmail.com>
2024-12-29 00:14:05 +0530

    Update primary color in Tailwind CSS configuration to a specific hex value

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,7 +20,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#305CDE',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
