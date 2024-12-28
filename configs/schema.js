9b408c24351fd330e1eccae622aa7acc562630ad
ritikdevelop <hrithikrana216@gmail.com>
2024-12-29 01:55:06 +0530

    Rename userName field to name in USER_TABLE schema for clarity

--- configs/schema.js
+++ configs/schema.js
@@ -2,7 +2,7 @@ import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const USER_TABLE = pgTable("users", {
   id: serial().primaryKey(),
-  userName: varchar().notNull(),
+  name: varchar().notNull(),
   email: varchar().notNull(),
   isMember: boolean().notNull().default(false),
 });
