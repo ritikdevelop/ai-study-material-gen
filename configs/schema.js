a37d447b3d4fe4bf009be027b40c64cbaef9efc2
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 00:19:23 +0530

    Add STUDY_MATERIAL_TABLE schema for managing study materials

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,19 @@
-import { boolean, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { boolean, json, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const USER_TABLE = pgTable("users", {
   id: serial().primaryKey(),
   name: varchar().notNull(),
   email: varchar().notNull(),
   isMember: boolean().notNull().default(false),
+});
+
+export const STUDY_MATERIAL_TABLE = pgTable("studyMaterial", {
+  id: serial().primaryKey(),
+  courseId: varchar().notNull(),
+  courseType: varchar().notNull(),
+  topic: varchar().notNull(),
+  difficultyLevel: varchar().default("Easy"),
+  courseLayout: json(),
+  createdBy: varchar().notNull(),
+  status: varchar().default("Pending"),
 });
