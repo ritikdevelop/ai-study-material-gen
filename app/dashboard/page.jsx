79859a6f4e6bf713781b305e3794440ada007e77
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 00:20:21 +0530

    Add CourseList component to Dashboard for displaying user's study materials

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import React from 'react'
 import WelcomeBanner from './_components/WelcomeBanner'
+import CourseList from './_components/CourseList';
 
 function Dashboard() {
   return (
     <div>
       <WelcomeBanner />
+
+      <CourseList />
     </div>
   )
 }
