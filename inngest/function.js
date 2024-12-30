0f8c07938da3375aacd10cf4db2e45d53a5d9e72
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 16:45:43 +0530

    feat: Implement GenerateNotes function to create chapter notes using AI and update course status to "Ready"

--- inngest/function.js
+++ inngest/function.js
@@ -1,7 +1,9 @@
 import { db } from "@/configs/db";
 import { inngest } from "./client";
-import { USER_TABLE } from "@/configs/schema";
+import { CHAPTER_NOTES_TABLE, USER_TABLE } from "@/configs/schema";
 import { eq } from "drizzle-orm";
+import { STUDY_MATERIAL_TABLE } from "@/configs/schema";
+import { courseOutlineAIModel, generateNotesAIModel } from "@/configs/AiModal";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -47,4 +49,49 @@ export const CreateNewUser = inngest.createFunction(
   }
 
   //   Step is to send s
+);
+
+export const GenerateNotes = inngest.createFunction(
+  { id: "generate-course" },
+  { event: "notes.generate" },
+  async ({ event, step }) => {
+    const { course } = event.data;
+
+    //! Generate the Notes for Each chapter with AI
+    const notesResult = await step.run("Generate Chapter Notes", async () => {
+      const Chapters = course?.courseLayout.chapters;
+      let index = 0;
+      Chapters.forEach(async (chapter) => {
+        const PROMPT =
+          "Generate exam material detail content for each chapter , Make sure to includes all topic point in the content, make sure to give content in HTML format (Do not Add HTMLK , Head, Body, title tag), The chapters : " +
+          JSON.stringify(chapter);
+
+        const result = await generateNotesAIModel.sendMessage(PROMPT);
+        const aiResp = result.response.text();
+
+        await db.insert(CHAPTER_NOTES_TABLE).values({
+          chapterId: index,
+          courseId: course?.courseId,
+          notes: aiResp,
+        });
+        index = index + 1;
+      });
+      return "Completed";
+    });
+
+    //! Update Status to Generating
+    const updateCourseStatusResult = await step.run(
+      "Update Course Status to Ready",
+      async () => {
+        const result = await db
+          .update(STUDY_MATERIAL_TABLE)
+          .set({
+            status: "Ready",
+          })
+          .where(eq(STUDY_MATERIAL_TABLE.courseId, course?.courseId));
+
+        return "Success";
+      }
+    );
+  }
 );
