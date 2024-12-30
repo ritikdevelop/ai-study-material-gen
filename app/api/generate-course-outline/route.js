6ac7cd306b7eb8d4d6fd2b6d18663034690080dd
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 16:46:55 +0530

    Enhance course outline generation to include chapter emojis and trigger notes generation via Inngest

--- app/api/generate-course-outline/route.js
+++ app/api/generate-course-outline/route.js
@@ -1,6 +1,7 @@
 import { courseOutlineAIModel } from "@/configs/AiModal";
 import { db } from "@/configs/db";
 import { STUDY_MATERIAL_TABLE } from "@/configs/schema";
+import { inngest } from "@/inngest/client";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -14,12 +15,17 @@ export async function POST(req) {
     courseType +
     " and level of difficulty will be " +
     difficultyLevel +
-    " with summary of course, List of Chapters along with summary  for each chapter, Topic list in each chapter all result in JSON format";
+    " with summary of course, List of Chapters along with summary and Emoji icon for each chapter, Topic list in each chapter all result in JSON format";
 
   //! Generate the course layout based on the user input
   const aiResp = await courseOutlineAIModel.sendMessage(PROMPT);
 
   const aiResult = JSON.parse(aiResp.response.text());
+
+  //! Ensure courseType is not null or undefined
+  // if (!courseType) {
+  //   throw new Error("courseType cannot be null or undefined");
+  // }
 
   //! Save the course layout to the database
   const dbResult = await db
@@ -32,7 +38,15 @@ export async function POST(req) {
       courseLayout: aiResult,
     })
     .returning({ STUDY_MATERIAL_TABLE });
-  console.log(dbResult);
+
+  //! Trigger the Inngest function to generate Chapter notes
+  const result = await inngest.send({
+    name: "notes.generate",
+    data: {
+      course: dbResult[0],
+    },
+  });
+  console.log(result);
 
   return NextResponse.json({ result: dbResult[0] });
 }
