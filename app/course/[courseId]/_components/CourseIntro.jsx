72ab059802b17a3c1d5e793c82a9d128d51f43c6
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 16:47:13 +0530

    feat: Add CourseIntro component to display course title, summary, and chapter count

---
+++ app/course/[courseId]/_components/CourseIntro.jsx
@@ -0,0 +1,24 @@
+import { Progress } from "@/components/ui/progress";
+import Image from "next/image";
+import React from "react";
+
+function CourseIntro({ course }) {
+  return (
+    <div className="flex gap-5 items-center p-10 border shadow-md rounded-lg">
+      <Image src={"/assets/knowledge.png"} alt="other" width={70} height={70} />
+      <div>
+        <h2 className="font-bold text-2xl">
+          {course?.courseLayout.course_title}
+        </h2>
+        <p>{course?.courseLayout?.summary}</p>
+        <Progress className="mt-3" />
+
+        <h2 className="mt-3 text-lg text-primary">
+          Total Chapter: {course?.courseLayout?.chapters?.length}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default CourseIntro;
