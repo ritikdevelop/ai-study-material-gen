4517b5e433bd94756cbad3e9d52783ce2ddcf193
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 16:48:21 +0530

    Add refresh button to CourseList for reloading course data

--- app/dashboard/_components/CourseList.jsx
+++ app/dashboard/_components/CourseList.jsx
@@ -4,35 +4,60 @@ import { useUser } from "@clerk/nextjs";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CourseCardItem from "./CourseCardItem";
+import { Button } from "@/components/ui/button";
+import { RefreshCw } from "lucide-react";
 
 function CourseList() {
   const { user } = useUser();
   const [courseList, setCourseList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     user && GetCourseList();
   }, [user]);
 
   const GetCourseList = async () => {
+    setLoading(true);
     const result = await axios.post("/api/courses", {
       createdBy: user?.primaryEmailAddress?.emailAddress,
     });
     console.log(result);
     setCourseList(result.data);
+    setLoading(false);
   };
   return (
     <div className="mt-10">
-      <h2 className="font-bold text-2xl">Your Study Material</h2>
+      <h2 className="font-bold text-2xl flex justify-between items-center">
+        Your Study Material
+        <Button
+          variant="outline"
+          onClick={GetCourseList}
+          className="border-primary text-primary"
+        >
+          {" "}
+          <RefreshCw /> Refresh{" "}
+        </Button>
+      </h2>
 
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-5">
         {/* {courseList?.map((course, index) => (
           <CourseCardItem course={course} key={index} />
         ))} */}
-        {Array.isArray(courseList) ? (
-          courseList.map((course, index) => (
-            <CourseCardItem course={course} key={index} />
-          ))
+        {loading == false ? (
+          Array.isArray(courseList) ? (
+            courseList.map((course, index) => (
+              <CourseCardItem course={course} key={index} />
+            ))
+          ) : (
+            <p>No courses available</p>
+          )
         ) : (
-          <p>No courses available</p>
+          [1, 2, 3, 4, 5, 6].map((item, index) => (
+            <div
+              key={index}
+              className="h-56 w-full bg-slate-200 rounded-lg animate-pulse"
+            ></div>
+          ))
         )}
       </div>
     </div>
