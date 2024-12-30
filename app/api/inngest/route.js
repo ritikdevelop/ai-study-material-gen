7b6c4df7c2506cf4fb5c7c2e52febfe634e30b43
ritikdevelop <hrithikrana216@gmail.com>
2024-12-30 16:46:30 +0530

    Add GenerateNotes function to API for chapter note generation

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { CreateNewUser, helloWorld } from "@/inngest/function";
+import { CreateNewUser, GenerateNotes, helloWorld } from "@/inngest/function";
 
-// Create an API that serves zero functions
+//! Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
     helloWorld,
-    CreateNewUser
+    CreateNewUser,
+    GenerateNotes
   ],
 });
