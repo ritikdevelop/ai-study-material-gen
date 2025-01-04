de06e39c6a1206540cff7b817a50ca1804490954
ritikdevelop <hrithikrana216@gmail.com>
2025-01-04 22:40:17 +0530

    refactor: Format isProtectedRoute for improved readability

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", '/create',"/course(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/create",
+  "/course(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
