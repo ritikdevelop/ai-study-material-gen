c8ca65c48ea09620e8ba68fa9937e8d315decaff
ritikdevelop <hrithikrana216@gmail.com>
2025-01-04 22:42:10 +0530

    refactor: Improve code formatting and consistency in DashboardHeader component

--- app/dashboard/_components/DashboardHeader.jsx
+++ app/dashboard/_components/DashboardHeader.jsx
@@ -1,12 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 function DashboardHeader() {
   return (
-    <div className='p-5 shadow-sm flex justify-end'>
+    <div className="p-5 shadow-sm flex justify-end">
       <UserButton />
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
